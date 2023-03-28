458ec4a0959a3ee5c36556e17cfc8513a4697a17
MirAn55500 <andrey_mironov02@mail.ru>
2023-03-28 13:32:21 +0700

    Если вошёл на сайт, отображение меняется (надеюсь)

--- application/app/urls.py
+++ application/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('home', views.HomeView.as_view(), name='home'),
     path('about', views.AboutView.as_view(), name='about'),
     path('personal_page/<int:id>', views.PersonalPage.as_view(), name='personal_page')
 ]
